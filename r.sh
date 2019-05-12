1cd2930f9bc936b73706eac9e0172fc710aaffa5
Gleb Zernov <gzernov@protonmail.com>
2019-05-12 17:57:17 +0300

    Initial commit
    - Copied template from https://github.com/mbuzdalov/latex-styles

---
+++ r.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ "$1" == "clean" ]]; then
+    rm -f {bachelor,master}-thesis.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct,pdf,out}
+else
+    for i in bachelor ; do
+        xelatex $i-thesis
+        biber   $i-thesis
+        xelatex $i-thesis
+        xelatex $i-thesis
+    done
+
+    rm -f bachelor-thesis.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct,out}
+
+    opera bachelor-thesis.pdf
+fi
